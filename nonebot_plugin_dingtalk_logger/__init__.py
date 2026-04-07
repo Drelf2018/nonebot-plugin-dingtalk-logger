6bec31ea1de4fe7f0bb9ac7381d057b9089b7513
12138 <41439182+Drelf2018@users.noreply.github.com>
2026-04-07 17:38:21 +0800

    fix: 修复插件信息缺失

--- nonebot_plugin_dingtalk_logger/__init__.py
+++ nonebot_plugin_dingtalk_logger/__init__.py
@@ -8,6 +8,7 @@ __plugin_meta__ = PluginMetadata(
     description="在使用 nonebot 自带的 logger 写入 ERROR 级别以上的日志时，会自动发送至钉钉机器人",
     usage="直接调用 logger 的方法",
     type="library",
+    homepage="https://github.com/Drelf2018/nonebot-plugin-dingtalk-logger",
     config=Bot,
 )
 
